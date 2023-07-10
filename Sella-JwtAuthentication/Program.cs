425cfb3bc75c5b635c8faad18153e5d66fba9076
Yusuf Mustafa <yusufmustafa1@outlook.com>
2023-07-10 06:06:30 +0300

    Add services, jwt changes, Cros, and Policy

--- Sella-JwtAuthentication/Program.cs
+++ Sella-JwtAuthentication/Program.cs
@@ -1,11 +1,55 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Sella_JwtAuthentication.Helpers;
+using Sella_JwtAuthentication.Models;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Add services to the container.
+builder.Services.Configure<JWTCONFIG>(builder.Configuration.GetSection("JWT"));
+builder.Services.AddDbContext<ApiDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//add authontiation
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(jwt =>
+    {
+        var key = Encoding.ASCII.GetBytes(builder.Configuration.GetSection("JWT:Key").Value);
+        jwt.SaveToken = true;
+        jwt.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = true,
+            ValidateAudience = false,
+            RequireExpirationTime = false,
+            ValidateLifetime = false
+        };
+    });
+
+builder.Services.AddCors(options => options.AddPolicy("frontEnd", configurePolicy =>
+{
+    configurePolicy.WithOrigins("http://localhost:59592").AllowAnyMethod().AllowAnyHeader();
+}));
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApiDbContext>();
 
 var app = builder.Build();
 
@@ -18,8 +62,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
-
+app.UseCors("frontEnd");
 app.MapControllers();
 
 app.Run();
