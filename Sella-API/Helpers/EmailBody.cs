9ad3e39b808ac96d89c4f6c0f289710a7e7d6f4e
Zeyad Mohsen <127048807+zeyadMohsenHegazy@users.noreply.github.com>
2023-07-21 15:20:38 +0300

    edite email body

--- Sella-API/Helpers/EmailBody.cs
+++ Sella-API/Helpers/EmailBody.cs
@@ -8,14 +8,15 @@
                     <head>
                     </head>
                     <body>
-                     <h1>Reset Your Password</h1>
+                        <div style=""border: 3px solid #026d02;width: fit-content;"">
+                     <h1 style=""margin-left: 3.5em;"">Reset Your Password</h1>
+                    <h3 style=""padding-left: 1em;padding-right: 1em;"">You are recieving this email as you asked for a password reset</h3> 
                     <br>
-                    <p>You are recieving this email as you asked for a password reset</p>
-                    <br>
-                    <a href=""http://localhost:51728/reset?email={email}&code={emailtoken}"" target=""_blank"">Reset Password</a>
+                    <a href=""http://localhost:4200/reset?email={email}&code={emailtoken}"" target=""_blank"" style=""padding: 20px;border: 5px solid #15c535;background-color: #15c535;color: #fff; font-weight: bold; text-decoration: none; font-size: larger; margin-left: 8.5em;"">Reset Password</a>
                     <br>
                     <br>
-                    <p>With Regards Sella Team</p>
+                    <h4 style=""margin-left: 1em;"">With Regards Sella Technical Team..</h4>
+                    </div>
                     </body>
                     </html>";
         }
